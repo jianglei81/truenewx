6e14132fa069d3f87c422ef9dacb7f1f35a0edf1
jianglei <jianglei@belfool>
2017-01-14 22:13:18 +0800

    恢复复合异步WEB请求拦截器

--- truenewx-web/src/main/java/org/truenewx/web/spring/context/request/CompositeAsyncWebRequestInterceptor.java
+++ truenewx-web/src/main/java/org/truenewx/web/spring/context/request/CompositeAsyncWebRequestInterceptor.java
@@ -4,20 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.AsyncWebRequestInterceptor;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
 
 /**
- * 复合的Web请求拦截器
+ * 复合的异步Web请求拦截器
  *
  * @author jianglei
  * @since JDK 1.8
  */
-public class CompositeWebRequestInterceptor implements WebRequestInterceptor {
+public class CompositeAsyncWebRequestInterceptor implements AsyncWebRequestInterceptor {
 
-    private List<WebRequestInterceptor> interceptors = new ArrayList<>();
+    private List<AsyncWebRequestInterceptor> interceptors = new ArrayList<>();
 
-    public void setInterceptors(final List<WebRequestInterceptor> interceptors) {
+    public void setInterceptors(final List<AsyncWebRequestInterceptor> interceptors) {
         this.interceptors = interceptors;
     }
 
@@ -42,10 +43,10 @@ public class CompositeWebRequestInterceptor implements WebRequestInterceptor {
         }
     }
 
+    @Override
     public void afterConcurrentHandlingStarted(final WebRequest request) {
         for (int i = this.interceptors.size() - 1; i >= 0; i--) {
-            // TODO spring版本升级到3.2或以上时，恢复成实现AsyncWebRequestInterceptor，再恢复代码
-            // this.interceptors.get(i).afterConcurrentHandlingStarted(request);
+            this.interceptors.get(i).afterConcurrentHandlingStarted(request);
         }
     }
 
