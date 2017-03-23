4a4c518365e31511986d2f6d7dbdb188dadeef5f
jiangnanyuzi <jiangnanyuzi@163.com>
2017-03-23 14:33:38 +0800

    修正前一个URL可能为null的问题

--- truenewx-web/src/main/java/org/truenewx/web/tag/PrevUrlTag.java
+++ truenewx-web/src/main/java/org/truenewx/web/tag/PrevUrlTag.java
@@ -34,24 +34,26 @@ public class PrevUrlTag extends TagSupport {
     public int doEndTag() throws JspException {
         final HttpServletRequest request = (HttpServletRequest) this.pageContext.getRequest();
         final String currentAction = WebUtil.getRelativeRequestAction(request);
-        String previousUrl = WebUtil.getRelativePreviousUrl(request, true);
-        if (previousUrl.startsWith(currentAction)) { // 如果前一页url以当前action开头，则执行默认的前一页规则，以避免跳转相同页
-            previousUrl = null;
-        } else {
-            final ApplicationContext context = SpringWebUtil.getApplicationContext(request);
-            final Loginer loginer = SpringUtil.getFirstBeanByClass(context, Loginer.class);
-            if (loginer != null && previousUrl != null && loginer.isLoginUrl(previousUrl)) {
-                previousUrl = null;
+        String prevUrl = WebUtil.getRelativePreviousUrl(request, true);
+        if (prevUrl != null) {
+            if (prevUrl.startsWith(currentAction)) { // 如果前一页url以当前action开头，则执行默认的前一页规则，以避免跳转相同页
+                prevUrl = null;
+            } else {
+                final ApplicationContext context = SpringWebUtil.getApplicationContext(request);
+                final Loginer loginer = SpringUtil.getFirstBeanByClass(context, Loginer.class);
+                if (loginer != null && prevUrl != null && loginer.isLoginUrl(prevUrl)) {
+                    prevUrl = null;
+                }
             }
         }
         final JspWriter out = this.pageContext.getOut();
         try {
-            if (previousUrl != null) {
+            if (prevUrl != null) {
                 final String contextPath = request.getContextPath();
                 if (!contextPath.equals(Strings.SLASH)) {
                     out.print(contextPath);
                 }
-                out.print(previousUrl);
+                out.print(prevUrl);
             } else {
                 out.print(this.defaultHref);
             }
