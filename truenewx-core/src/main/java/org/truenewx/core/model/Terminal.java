243c2ed41caf16343fd608cd7e1465dcc15eb38d
jiangnanyuzi <jiangnanyuzi@163.com>
2017-12-15 17:01:25 +0800

    终端类型增强辅助能力

--- truenewx-core/src/main/java/org/truenewx/core/model/Terminal.java
+++ truenewx-core/src/main/java/org/truenewx/core/model/Terminal.java
@@ -1,8 +1,11 @@
 package org.truenewx.core.model;
 
+import org.apache.commons.lang3.EnumUtils;
+import org.truenewx.core.Strings;
 import org.truenewx.core.enums.Device;
 import org.truenewx.core.enums.OS;
 import org.truenewx.core.enums.Program;
+import org.truenewx.core.util.ArrayUtil;
 
 /**
  * 终端类型
@@ -27,21 +30,17 @@ public class Terminal {
      */
     private Program program;
 
-    /**
-     *
-     * @return 操作系统
-     */
-    public OS getOs() {
-        return this.os;
+    public Terminal(final Device device, final OS os, final Program program) {
+        this.device = device;
+        this.os = os;
+        this.program = program;
     }
 
-    /**
-     *
-     * @param os
-     *            操作系统
-     */
-    public void setOs(final OS os) {
-        this.os = os;
+    public Terminal(final String s) {
+        final String[] array = s.split(Strings.DOT);
+        this.device = EnumUtils.getEnum(Device.class, ArrayUtil.get(array, 0));
+        this.os = EnumUtils.getEnum(OS.class, ArrayUtil.get(array, 1));
+        this.program = EnumUtils.getEnum(Program.class, ArrayUtil.get(array, 2));
     }
 
     /**
@@ -52,11 +51,11 @@ public class Terminal {
     }
 
     /**
-     * @param device
-     *            设备类型
+     *
+     * @return 操作系统
      */
-    public void setDevice(final Device device) {
-        this.device = device;
+    public OS getOs() {
+        return this.os;
     }
 
     /**
@@ -66,11 +65,37 @@ public class Terminal {
         return this.program;
     }
 
-    /**
-     * @param program
-     *            程序类型
-     */
-    public void setProgram(final Program program) {
-        this.program = program;
+    public boolean supports(final Terminal terminal) {
+        // 设备属性为null，视为支持所有设备
+        if (this.device != null && this.device != terminal.device) {
+            return false;
+        }
+        // 操作系统属性为null，视为支持所有操作系统
+        if (this.os != null && this.os != terminal.os) {
+            return false;
+        }
+        // 程序类型属性为null，视为支持所有程序类型
+        if (this.program != null && this.program != terminal.program) {
+            return false;
+        }
+        return true;
     }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer();
+        if (this.device != null) {
+            sb.append(this.device.name());
+        }
+        sb.append(Strings.DOT);
+        if (this.os != null) {
+            sb.append(this.os.name());
+        }
+        sb.append(Strings.DOT);
+        if (this.program != null) {
+            sb.append(this.program.name());
+        }
+        return sb.toString();
+    }
+
 }
