f550934fbb5babd75297ea99106f1305f97f013c
jiangnanyuzi <jiangnanyuzi@163.com>
2018-12-20 16:47:13 +0800

    从切分者获取HibernateTemplate更改为从实体名称获取，以略微提高性能并简化代码编写

--- truenewx-data-hibernate/src/main/java/org/truenewx/data/orm/dao/support/hibernate/HibernateSlicedDaoSupoort.java
+++ truenewx-data-hibernate/src/main/java/org/truenewx/data/orm/dao/support/hibernate/HibernateSlicedDaoSupoort.java
@@ -16,61 +16,59 @@ import org.truenewx.data.query.QueryResult;
  *
  * @author jianglei
  * @since JDK 1.8
- * @param <T>
- *            实体类型
- * @param <S>
- *            切分者类型
+ * @param <T> 实体类型
+ * @param <S> 切分者类型
  */
 public abstract class HibernateSlicedDaoSupoort<T extends SlicedEntity<S>, S extends Serializable>
         extends HibernateEntityDaoSupport<T> implements SlicedDao<T, S> {
 
-    protected final HibernateTemplate getHibernateTemplate(final S slicer) {
-        final String entityName = getEntityName(slicer);
+    protected HibernateTemplate getHibernateTemplate(String entityName) {
         return getDataAccessTemplate(entityName);
     }
 
     @Override
-    public final void delete(final T entity) {
+    public void delete(T entity) {
         if (entity != null) {
-            final S slicer = entity.getSlicer();
+            S slicer = entity.getSlicer();
             if (slicer != null) {
-                final String entityName = getEntityName(slicer);
+                String entityName = getEntityName(slicer);
                 getDataAccessTemplate(entityName).getSession().delete(entityName, entity);
             }
         }
     }
 
     @Override
-    public final void save(final T entity) {
+    public void save(T entity) {
         if (entity != null) {
-            final S slicer = entity.getSlicer();
-            final String entityName = getEntityName(slicer);
+            S slicer = entity.getSlicer();
+            String entityName = getEntityName(slicer);
             getDataAccessTemplate(entityName).getSession().saveOrUpdate(entityName, entity);
         }
     }
 
     @Override
-    public final void refresh(final T entity) {
+    public void refresh(T entity) {
         if (entity != null) {
-            final S slicer = entity.getSlicer();
+            S slicer = entity.getSlicer();
             if (slicer != null) {
-                final String entityName = getEntityName(slicer);
+                String entityName = getEntityName(slicer);
                 getDataAccessTemplate(entityName).getSession().refresh(entityName, entity);
             }
         }
     }
 
     @Override
-    public final void flush(final S slicer) {
+    public void flush(S slicer) {
         if (slicer != null) {
-            getHibernateTemplate(slicer).getSession().flush();
+            String entityName = getEntityName(slicer);
+            getHibernateTemplate(entityName).getSession().flush();
         }
     }
 
     @Override
-    public final List<T> find(final Map<String, ?> params, final String... fuzzyNames) {
-        final List<T> list = new ArrayList<>();
-        for (final S slice : getSlices()) {
+    public List<T> find(Map<String, ?> params, String... fuzzyNames) {
+        List<T> list = new ArrayList<>();
+        for (S slice : getSlices()) {
             list.addAll(find(getEntityName(slice), params, fuzzyNames));
         }
         return list;
@@ -79,7 +77,7 @@ public abstract class HibernateSlicedDaoSupoort<T extends SlicedEntity<S>, S ext
     @Override
     public int countAll() {
         int count = 0;
-        for (final S slice : getSlices()) {
+        for (S slice : getSlices()) {
             count += countAll(getEntityName(slice));
         }
         return count;
@@ -88,11 +86,10 @@ public abstract class HibernateSlicedDaoSupoort<T extends SlicedEntity<S>, S ext
     /**
      * 根据切分者获取实体名称
      *
-     * @param slicer
-     *            切分者
+     * @param slicer 切分者
      * @return 实体名称
      */
-    protected abstract String getEntityName(final S slicer);
+    protected abstract String getEntityName(S slicer);
 
     /**
      * 获取全部切片清单，通过以该清单中的切片作为切分者可以遍历所有切分表
@@ -107,30 +104,28 @@ public abstract class HibernateSlicedDaoSupoort<T extends SlicedEntity<S>, S ext
      * 根据被依赖实体获取切分者标识<br/>
      * 一般情况下，切分实体所依赖的实体即为切分者，如有特殊，子类可覆写
      *
-     * @param dependedClass
-     *            被依赖实体的类型
-     * @param dependedKey
-     *            被依赖实体的标识
+     * @param dependedClass 被依赖实体的类型
+     * @param dependedKey   被依赖实体的标识
      * @return 切分者标识
      */
     @SuppressWarnings("unchecked")
-    protected S getSlicer(final Class<?> dependedClass, final Serializable dependedKey) {
+    protected S getSlicer(Class<?> dependedClass, Serializable dependedKey) {
         return (S) dependedKey;
     }
 
     @Override
-    public T first(final S slicer) {
+    public T first(S slicer) {
         return first(getEntityName(slicer));
     }
 
-    public QueryResult<T> find(final Class<?> dependedClass, final Serializable dependedKey,
-            final QueryParameter parameter) {
-        final String entityName = getEntityName(getSlicer(dependedClass, dependedKey));
+    public QueryResult<T> find(Class<?> dependedClass, Serializable dependedKey,
+            QueryParameter parameter) {
+        String entityName = getEntityName(getSlicer(dependedClass, dependedKey));
         return find(entityName, dependedClass, dependedKey, parameter);
     }
 
-    public int delete(final Class<?> dependedClass, final Serializable dependedKey) {
-        final String entityName = getEntityName(getSlicer(dependedClass, dependedKey));
+    public int delete(Class<?> dependedClass, Serializable dependedKey) {
+        String entityName = getEntityName(getSlicer(dependedClass, dependedKey));
         return delete(entityName, dependedClass, dependedKey);
     }
 }
