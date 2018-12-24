4bbac53a08255804595e20648675c40128ac2d9f
jiangnanyuzi <jiangnanyuzi@163.com>
2018-12-24 17:56:51 +0800

    添加解析字符串为时间类型的方法

--- truenewx-core/src/main/java/org/truenewx/core/util/TemporalUtil.java
+++ truenewx-core/src/main/java/org/truenewx/core/util/TemporalUtil.java
@@ -7,7 +7,10 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.Temporal;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Java 8 新版时间相关类的工具类
@@ -53,10 +56,8 @@ public class TemporalUtil {
     /**
      * 按照指定格式格式化时间点对象为字符串型日期
      *
-     * @param temporal
-     *            时间点
-     * @param pattern
-     *            日期格式
+     * @param temporal 时间点
+     * @param pattern  日期格式
      * @return 字符串型日期
      */
     public static String format(Temporal temporal, String pattern) {
@@ -83,8 +84,7 @@ public class TemporalUtil {
     /**
      * 按照短日期格式(yyyy-MM-dd)格式化时间点对象为字符串型日期
      *
-     * @param instant
-     *            时间点
+     * @param instant 时间点
      * @return 字符串型日期
      */
     public static String formatShort(Instant instant) {
@@ -98,8 +98,7 @@ public class TemporalUtil {
     /**
      * 按照长日期格式(yyyy-MM-dd HH:mm:ss)转换时间点对象为字符串型日期
      *
-     * @param instant
-     *            时间点
+     * @param instant 时间点
      * @return 字符串型日期
      */
     public static String formatLong(Instant instant) {
@@ -109,8 +108,7 @@ public class TemporalUtil {
     /**
      * 按照长日期格式(yyyyMMddHHmmss)转换时间点对象为字符串型日期
      *
-     * @param instant
-     *            时间点
+     * @param instant 时间点
      * @return 字符串型日期
      */
     public static String formatLongNoDelimiter(Instant instant) {
@@ -119,11 +117,9 @@ public class TemporalUtil {
 
     /**
      * 计算指定两个时间之间的相差天数。如果earlierTime晚于laterTime，则返回负值
-     * 
-     * @param earlierTime
-     *            较早时间
-     * @param laterTime
-     *            较晚时间
+     *
+     * @param earlierTime 较早时间
+     * @param laterTime   较晚时间
      * @return 相差天数
      */
     public static int daysBetween(Instant earlierTime, Instant laterTime) {
@@ -135,10 +131,8 @@ public class TemporalUtil {
     /**
      * 计算指定两个日期之间的相差天数。如果earlierDate晚于laterDate，则返回负值
      *
-     * @param earlierDate
-     *            较早日期
-     * @param laterDate
-     *            较晚日期
+     * @param earlierDate 较早日期
+     * @param laterDate   较晚日期
      * @return 相差天数
      */
     public static int daysBetween(LocalDate earlierDate, LocalDate laterDate) {
@@ -160,16 +154,11 @@ public class TemporalUtil {
     /**
      * 为指定时间点设置时分秒纳秒，返回新日期
      *
-     * @param instant
-     *            原时间点
-     * @param hour
-     *            时
-     * @param minute
-     *            分
-     * @param second
-     *            秒
-     * @param nanoOfSecond
-     *            纳秒
+     * @param instant      原时间点
+     * @param hour         时
+     * @param minute       分
+     * @param second       秒
+     * @param nanoOfSecond 纳秒
      * @return 新时间点
      */
     public static Instant setTime(Instant instant, int hour, int minute, int second,
@@ -178,6 +167,50 @@ public class TemporalUtil {
         dateTime = LocalDateTime.of(dateTime.getYear(), dateTime.getMonthValue(),
                 dateTime.getDayOfMonth(), hour, minute, second, nanoOfSecond);
         return toInstant(dateTime);
+    }
+
+    public static Instant parseInstant(String s) {
+        if (StringUtils.isNotBlank(s)) {
+            try {
+                return Instant.parse(s);
+            } catch (DateTimeParseException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public static LocalDate parseDate(String s) {
+        if (StringUtils.isNotBlank(s)) {
+            try {
+                return LocalDate.parse(s);
+            } catch (DateTimeParseException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public static LocalTime parseTime(String s) {
+        if (StringUtils.isNotBlank(s)) {
+            try {
+                return LocalTime.parse(s);
+            } catch (DateTimeParseException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public static LocalDateTime parseDateTime(String s) {
+        if (StringUtils.isNotBlank(s)) {
+            try {
+                return LocalDateTime.parse(s);
+            } catch (DateTimeParseException e) {
+                return null;
+            }
+        }
+        return null;
     }
 
 }
