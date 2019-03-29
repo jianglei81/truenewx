9d281a8a2edf4f0976925a533593297cb009537d
jiangnanyuzi <jiangnanyuzi@163.com>
2019-03-29 11:33:29 +0800

    添加根据单体id比较，判断指定单体集合中是否包含指定单体的方法

--- truenewx-data/src/main/java/org/truenewx/data/model/unity/UnityUtil.java
+++ truenewx-data/src/main/java/org/truenewx/data/model/unity/UnityUtil.java
@@ -21,8 +21,7 @@ public class UnityUtil {
     /**
      * 获取指定单体集合的id集合
      *
-     * @param unities
-     *            单体集合
+     * @param unities 单体集合
      * @return id集合
      */
     public static <K extends Serializable> Set<K> getIdSet(Collection<? extends Unity<K>> unities) {
@@ -36,10 +35,8 @@ public class UnityUtil {
     /**
      * 判断指定单体集合中是否包含指定id的单体
      *
-     * @param unities
-     *            单体集合
-     * @param id
-     *            单体id
+     * @param unities 单体集合
+     * @param id      单体id
      * @return 指定单体集合中是否包含指定id的单体
      */
     public static <T extends Unity<K>, K extends Serializable> boolean containsId(
@@ -55,10 +52,28 @@ public class UnityUtil {
     }
 
     /**
+     * 根据单体id比较，判断指定单体集合中是否包含指定单体
+     *
+     * @param unities 单体集合
+     * @param unity   单体
+     * @return 指定单体集合中是否包含指定单体
+     */
+    public static <T extends Unity<K>, K extends Serializable> boolean containsById(
+            Collection<T> unities, T unity) {
+        if (unities != null) {
+            for (Unity<K> u : unities) {
+                if (u.getId().equals(unity.getId())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
      * 将指定单体集合转换为以id为key，单体对象为value的Map映射集
      *
-     * @param collection
-     *            单体集合
+     * @param collection 单体集合
      * @return 单体映射集
      */
     public static <T extends Unity<K>, K extends Serializable> Map<K, T> toMap(
