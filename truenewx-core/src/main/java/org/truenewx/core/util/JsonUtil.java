d0e6ca92974c2cb9475edbbe35b8d273fd624bfc
jiangnanyuzi <jiangnanyuzi@163.com>
2019-09-06 21:52:09 +0800

    日期类型转换添加对Instant和LocalDateTime的支持

--- truenewx-core/src/main/java/org/truenewx/core/util/JsonUtil.java
+++ truenewx-core/src/main/java/org/truenewx/core/util/JsonUtil.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -35,26 +37,31 @@ import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
 public class JsonUtil {
 
     static {
-        final JsonDateCodec dateCodec = new JsonDateCodec();
-        SerializeConfig.getGlobalInstance().put(Date.class, dateCodec);
-        SerializeConfig.getGlobalInstance().put(java.sql.Date.class, dateCodec);
-        SerializeConfig.getGlobalInstance().put(Timestamp.class, dateCodec);
-        ParserConfig.getGlobalInstance().putDeserializer(Date.class, dateCodec);
-        ParserConfig.getGlobalInstance().putDeserializer(java.sql.Date.class, dateCodec);
-        ParserConfig.getGlobalInstance().putDeserializer(Timestamp.class, dateCodec);
-        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        JsonDateCodec dateCodec = new JsonDateCodec();
+        SerializeConfig serializeConfig = SerializeConfig.getGlobalInstance();
+        serializeConfig.put(Date.class, dateCodec);
+        serializeConfig.put(java.sql.Date.class, dateCodec);
+        serializeConfig.put(Timestamp.class, dateCodec);
+        serializeConfig.put(Instant.class, dateCodec);
+        serializeConfig.put(LocalDateTime.class, dateCodec);
+        ParserConfig parserConfig = ParserConfig.getGlobalInstance();
+        parserConfig.putDeserializer(Date.class, dateCodec);
+        parserConfig.putDeserializer(java.sql.Date.class, dateCodec);
+        parserConfig.putDeserializer(Timestamp.class, dateCodec);
+        parserConfig.putDeserializer(Instant.class, dateCodec);
+        parserConfig.putDeserializer(LocalDateTime.class, dateCodec);
+        parserConfig.setAutoTypeSupport(true);
     }
 
     /**
      * 获取JSON过滤器实例
      *
-     * @param clazz
-     *            需进行属性排除的类
-     * @param excludeProperties
-     *            需排除的属性
+     * @param clazz             需进行属性排除的类
+     * @param excludeProperties 需排除的属性
      * @return JSON过滤器实例
      */
-    private static PropertyPreFilter getFilterInstance(final Class<?> clazz, final String... excludeProperties) {
+    private static PropertyPreFilter getFilterInstance(final Class<?> clazz,
+            final String... excludeProperties) {
         final SimplePropertyPreFilter filter = new SimplePropertyPreFilter(clazz);
         final Set<String> excludeList = filter.getExcludes();
         for (final String exclude : excludeProperties) {
@@ -66,11 +73,11 @@ public class JsonUtil {
     /**
      * 获取JSON过滤器实例
      *
-     * @param filteredPropertiesMap
-     *            过滤属性映射集
+     * @param filteredPropertiesMap 过滤属性映射集
      * @return JSON过滤器实例
      */
-    private static PropertyPreFilter getFilterInstance(final Map<Class<?>, FilteredTokens> filteredPropertiesMap) {
+    private static PropertyPreFilter getFilterInstance(
+            final Map<Class<?>, FilteredTokens> filteredPropertiesMap) {
         final MultiPropertyPreFilter filter = new MultiPropertyPreFilter();
         for (final Entry<Class<?>, FilteredTokens> entry : filteredPropertiesMap.entrySet()) {
             final FilteredTokens value = entry.getValue();
@@ -82,38 +89,33 @@ public class JsonUtil {
     /**
      * 将指定任意类型的对象转换为JSON标准格式的字符串
      *
-     * @param obj
-     *            对象
-     * @param clazz
-     *            需进行属性排除的类
-     * @param excludeProperties
-     *            需排除的属性
+     * @param obj               对象
+     * @param clazz             需进行属性排除的类
+     * @param excludeProperties 需排除的属性
      * @return JSON格式的字符串
      */
-    public static String toJson(final Object obj, final Class<?> clazz, final String... excludeProperties) {
+    public static String toJson(final Object obj, final Class<?> clazz,
+            final String... excludeProperties) {
         return JSON.toJSONString(obj, getFilterInstance(clazz, excludeProperties));
     }
 
     /**
      * 将指定任意类型的对象转换为JSON标准格式的字符串
      *
-     * @param obj
-     *            对象
-     * @param filteredPropertiesMap
-     *            过滤属性映射集
+     * @param obj                   对象
+     * @param filteredPropertiesMap 过滤属性映射集
      * @return JSON格式的字符串
      */
-    public static String toJson(final Object obj, final Map<Class<?>, FilteredTokens> filteredPropertiesMap) {
+    public static String toJson(final Object obj,
+            final Map<Class<?>, FilteredTokens> filteredPropertiesMap) {
         return JSON.toJSONString(obj, getFilterInstance(filteredPropertiesMap));
     }
 
     /**
      * 将指定任意类型的对象转换为JSON标准格式的字符串
      *
-     * @param obj
-     *            对象
-     * @param appendTypeClasses
-     *            需要附加类型字段的类型集
+     * @param obj               对象
+     * @param appendTypeClasses 需要附加类型字段的类型集
      * @return JSON格式的字符串
      */
     public static String toJson(final Object obj, final Class<?>... appendTypeClasses) {
@@ -127,8 +129,7 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为对象
      *
-     * @param json
-     *            JSON标准形式的字符串
+     * @param json JSON标准形式的字符串
      * @return 转换形成的对象
      */
     public static Object json2Bean(final String json) {
@@ -138,10 +139,8 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为指定类型的对象
      *
-     * @param json
-     *            JSON标准形式的字符串
-     * @param clazz
-     *            要转换的类型
+     * @param json  JSON标准形式的字符串
+     * @param clazz 要转换的类型
      * @return 转换形成的对象
      */
     public static <T> T json2Bean(final String json, final Class<T> clazz) {
@@ -151,10 +150,8 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换生成的对象的所有属性值覆盖指定对象的相应属性值
      *
-     * @param json
-     *            JSON标准形式的字符串
-     * @param bean
-     *            要覆盖的对象
+     * @param json JSON标准形式的字符串
+     * @param bean 要覆盖的对象
      */
     public static void jsonCoverBean(final String json, final Object bean) {
         final JSONObject jsonObj = JSON.parseObject(json);
@@ -170,8 +167,7 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为具体类型不确定的List
      *
-     * @param json
-     *            JSON标准形式的字符串
+     * @param json JSON标准形式的字符串
      * @return 转换形成的对象List
      */
     public static List<Object> json2List(final String json) {
@@ -181,10 +177,8 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为指定类型的对象List
      *
-     * @param json
-     *            JSON标准形式的字符串
-     * @param clazz
-     *            元素类型
+     * @param json  JSON标准形式的字符串
+     * @param clazz 元素类型
      * @return 转换形成的对象List
      */
     public static <T> List<T> json2List(final String json, final Class<T> clazz) {
@@ -194,10 +188,8 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为指定类型的对象List
      *
-     * @param json
-     *            JSON标准形式的字符串
-     * @param types
-     *            依次为结果数组的每一个元素的类型，为空则不限制类型
+     * @param json  JSON标准形式的字符串
+     * @param types 依次为结果数组的每一个元素的类型，为空则不限制类型
      * @return 转换形成的对象List
      */
     public static List<Object> json2List(final String json, final Type[] types) {
@@ -211,8 +203,7 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为Map
      *
-     * @param json
-     *            JSON标准形式的字符串
+     * @param json JSON标准形式的字符串
      * @return 转换形成的Map
      */
     @SuppressWarnings("unchecked")
@@ -221,7 +212,8 @@ public class JsonUtil {
     }
 
     @SuppressWarnings("unchecked")
-    public static <K, V> Map<K, V> json2Map(final String json, final Class<K> keyClass, final Class<V> valueClass) {
+    public static <K, V> Map<K, V> json2Map(final String json, final Class<K> keyClass,
+            final Class<V> valueClass) {
         final Map<K, V> result = new HashMap<>();
         final Map<String, Object> map = JSON.parseObject(json);
         for (final Entry<String, Object> entry : map.entrySet()) {
@@ -240,8 +232,7 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为Properties
      *
-     * @param json
-     *            JSON标准形式的字符串
+     * @param json JSON标准形式的字符串
      * @return 转换形成的Properties
      */
     public static Properties json2Properties(final String json) {
@@ -251,8 +242,7 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为数组
      *
-     * @param json
-     *            JSON标准形式的字符串
+     * @param json JSON标准形式的字符串
      * @return 转换形成的数组
      */
     public static Object[] json2Array(final String json) {
@@ -263,10 +253,8 @@ public class JsonUtil {
     /**
      * 将JSON标准形式的字符串转换为数组
      *
-     * @param json
-     *            JSON标准形式的字符串
-     * @param clazz
-     *            元素类型
+     * @param json  JSON标准形式的字符串
+     * @param clazz 元素类型
      * @return 转换形成的数组
      */
     @SuppressWarnings("unchecked")
