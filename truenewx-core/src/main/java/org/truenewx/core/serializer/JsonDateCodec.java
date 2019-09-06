76aa1d8b681786c1d6529d1a10442464574b6918
jiangnanyuzi <jiangnanyuzi@163.com>
2019-09-06 22:30:02 +0800

    添加Instant和LocalDateTime的转换

--- truenewx-core/src/main/java/org/truenewx/core/serializer/JsonDateCodec.java
+++ truenewx-core/src/main/java/org/truenewx/core/serializer/JsonDateCodec.java
@@ -2,7 +2,11 @@ package org.truenewx.core.serializer;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import org.truenewx.core.util.TemporalUtil;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser;
 import com.alibaba.fastjson.serializer.JSONSerializer;
@@ -17,21 +21,26 @@ import com.alibaba.fastjson.serializer.LongCodec;
 public class JsonDateCodec extends LongCodec {
 
     @Override
-    public void write(final JSONSerializer serializer, final Object object, final Object fieldName,
-            final Type fieldType, final int features) throws IOException {
-        if (object == null) {
-            serializer.getWriter().writeNull();
-            return;
+    public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType,
+            int features) throws IOException {
+        if (object instanceof Date) {
+            Date date = (Date) object;
+            serializer.write(date.getTime());
+        } else if (object instanceof Instant) {
+            Instant instant = (Instant) object;
+            serializer.write(instant.toEpochMilli());
+        } else if (object instanceof LocalDateTime) {
+            LocalDateTime dateTime = (LocalDateTime) object;
+            serializer.write(TemporalUtil.toInstant(dateTime).toEpochMilli());
+        } else {
+            serializer.writeNull();
         }
-
-        final Date date = (Date) object;
-        serializer.write(date.getTime());
     }
 
     @Override
     @SuppressWarnings("unchecked")
-    public <T> T deserialze(final DefaultJSONParser parser, final Type type, final Object fieldName) {
-        final Long value = super.deserialze(parser, type, fieldName);
+    public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+        Long value = super.deserialze(parser, type, fieldName);
         return (T) new Date(value);
     }
 }
